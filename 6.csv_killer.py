0ae873d7ee8e298e536f4d095ea8aac27e65dd78
booooozaa <73605321+booooozaa@users.noreply.github.com>
2021-07-19 19:13:05 +0530

    Delete 6.csv_killer.py

--- 6.csv_killer.py
+++
@@ -1,7 +0,0 @@
-import csv
-import os
-watchlist = [28551, 28553, 28558, 28560,1250, 729, 2094, 62034, 2302, 4267, 9077, 36027, 1324, 2211, 3087, 1271, 1617, 1375]
-
-for instrument in watchlist:
-    file = f'{instrument}.csv'
-    os.remove(file)
